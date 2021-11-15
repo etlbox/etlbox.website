afab3a705a2fff8119ec5c89323f8d0be039c5f9
Andreas Lennartz <mail@etlbox.net>
2021-11-15 16:48:14 +0100

    Creating demo webservice servicing order data

---
+++ hugo/assets/lambda/orders.js
@@ -0,0 +1,61 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        "customerData": [
+          {
+            "OrderNumber": 10100,
+            "CustomerName": 1,
+            "Description": "T-Shirts: 7"
+          },
+          {
+            "OrderNumber": 10200,
+            "CustomerId": 2,
+            "Description": "Jeans: 12"
+          },
+          {
+            "OrderNumber": 10300,
+            "CustomerId": 3,
+            "Description": "Shoes: 15"
+          },
+          {
+            "OrderNumber": 10400,
+            "CustomerId": 1,
+            "Description": "Shoes: 15"
+          },
+          {
+            "OrderNumber": 10500,
+            "CustomerId": 2,
+            "Description": "Jeans: 3"
+          },
+          {
+            "OrderNumber": 10600,
+            "CustomerId": 3,
+            "Description": "T-Shirts: 4"
+          },
+          {
+            "OrderNumber": 10700,
+            "CustomerId": 1,
+            "Description": "Shoes: 10"
+          },
+          {
+            "OrderNumber": 10800,
+            "CustomerId": 2,
+            "Description": "Jeans: 3"
+          },
+          {
+            "OrderNumber": 10900,
+            "CustomerId": 3,
+            "Description": "T-Shirts: 10"
+          },
+          {
+            "OrderNumber": 11000,
+            "CustomerId": 1,
+            "Description": "T-Shirts: 10"
+          }
+        ]
+      }
+    )
+  };
+}
