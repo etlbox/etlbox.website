8fb686ac1cdf4de4cd4a1f63be4b9444cf8b2e10
Andreas Lennartz <mail@etlbox.net>
2023-01-30 23:03:31 +0100

    working searchbar, missing dark theme

--- newdoks/assets/js/index.js
+++ newdoks/assets/js/index.js
@@ -4,4 +4,14 @@ algoliasearchNetlify({
   siteId: 'c975bf38-6508-465e-98ed-0f61c02a80d9',
   branch: 'main',
   selector: 'div#search-box',
+  detached: false,
 });
+
+/*
+theme: {
+  mark: '#fff',
+  background: '#23263b',
+  selected: '#111432',
+  text: '#d6d6e7',
+  colorSourceIcon: '#d6d6e7'
+}*/
