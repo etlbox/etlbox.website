1c0a7f58f1c2a4594de1460d90ca5053c81d1b5c
Andreas Lennartz <mail@etlbox.net>
2023-10-04 10:03:18 +0200

    Adding api script for EFBox

---
+++ create_api_md/create_api_efbox.ps1
@@ -0,0 +1,40 @@
+#Preparations could be (untested)
+# dotnet tool update -g docfx
+# copy docfx.json into docfx_project folder
+# dotnet build DocFxToHugoMD 
+
+# We need to clean up the bin/obj folders (we also need to docfx cache folder stored there)
+# in order to make sure that the xdoc folders used by docfx are removed from the obj/bin folders, otherwise cached data is used which 
+# can lead to incorrect result
+
+# Approach 1 using dotnet clean
+#dotnet clean ..\..\etlbox.tests --configuration Debug
+#dotnet clean ..\..\etlbox.tests --configuration Release
+#dotnet clean ..\..\etlbox.tests --configuration DebNoLic
+#dotnet clean ..\..\etlbox.tests --configuration RelNoLic
+
+#dotnet clean does not always clean up properly, so let's do it the hard way
+$dir = dir ..\..\etlbox.source | ?{$_.PSISContainer}
+foreach ($d in $dir){
+    $binPath = Join-Path -Path $d.FullName -ChildPath \bin
+    $objPath = Join-Path -Path $d.FullName -ChildPath \obj
+    write-host "Removing folder $binPath"
+    write-host "Removing folder $objPath"
+    remove-item $binPath -Recurse
+    remove-item $objPath -Recurse
+}
+
+#The _site and some obj folder created by docfx may contain some cached date, so we remove it as well
+#remove-item ./obj -Recurse 
+remove-item docfx_project/obj -Recurse
+remove-item docfx_project/_site_efbox -Recurse
+
+#Then run the API migration (tested, should work like this)
+dotnet build ..\..\etlbox.tests --configuration Debug
+dotnet build apitransform\DocFxToHugoMD --configuration Debug
+
+docfx docfx_project/docfx_efbox.json --build
+./apitransform/DocFxToHugoMD/bin/Debug/net5.0/DocFxToHugoMD.exe
+remove-item 'C:\Users\andreaslennartz\Github\etlbox\efbox.website\doks_1.0\content\docs\api' -Recurse
+ove-item './Output/api' 'C:\Users\andreaslennartz\Github\etlbox\efbox.website\doks_1.5\content\docs\api' 
+pause
