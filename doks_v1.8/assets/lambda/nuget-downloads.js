b93df2fbca8979c9f550db72a6ce7071a625f7f0
Andreas Lennartz <mail@etlbox.net>
2024-12-27 23:51:28 +0100

    Adding download counter

---
+++ doks_v1.8/assets/lambda/nuget-downloads.js
@@ -0,0 +1,46 @@
+const https = require('https');
+
+const packages = ['ETLBox', 'ETLBox.SqlServer', 'ETLBox.Xml'];
+
+exports.handler = async () => {
+  try {
+    const totalDownloads = await fetchTotalDownloads(packages);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ totalDownloads }),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+  }
+};
+
+async function fetchTotalDownloads(packageNames) {
+  let totalDownloads = 0;
+
+  for (const packageName of packageNames) {
+    const downloads = await fetchNugetDownloads(packageName);
+    totalDownloads += downloads;
+  }
+
+  return totalDownloads;
+}
+
+function fetchNugetDownloads(packageName) {
+  const url = `https://api-v2v3search-0.nuget.org/query?q=${packageName}&prerelease=false`;
+
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+      res.on('data', (chunk) => (data += chunk));
+      res.on('end', () => {
+        const result = JSON.parse(data);
+        const packageInfo = result.data.find((pkg) => pkg.id.toLowerCase() === packageName.toLowerCase());
+        if (packageInfo) {
+          resolve(packageInfo.totalDownloads);
+        } else {
+          reject(new Error(`Package ${packageName} not found`));
+        }
+      });
+    }).on('error', reject);
+  });
+}
