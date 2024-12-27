6674a8efe66638d8161c4ba8b5f3148148710e43
Andreas Lennartz <mail@etlbox.net>
2024-12-28 00:33:33 +0100

    Update download counter

--- doks_v1.8/assets/lambda/nuget-downloads.js
+++ doks_v1.8/assets/lambda/nuget-downloads.js
@@ -1,26 +1,35 @@
 const https = require('https');
 
-// Liste der NuGet-Pakete
+// List of NuGet packages
 const packages = ['ETLBox', 'ETLBox.SqlServer', 'ETLBox.Json'];
 
-// Cache-Variable und Ablaufzeit (z.B. 12 Stunden)
+// Cache variable and expiration time (e.g., 12 hours)
 let cache = { downloads: 0, timestamp: 0 };
-const CACHE_DURATION = 12 * 60 * 60 * 1000; // 12 Stunden in Millisekunden
+const CACHE_DURATION = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
 
-exports.handler = async () => {
+exports.handler = async (event) => {
   try {
-    // Überprüfe, ob der Cache noch gültig ist
+    // Get the origin of the request
+    const allowedOrigins = ['http://localhost', 'http://localhost:1313', 'https://www.etlbox.net'];
+    const origin = event.headers.origin;
+    const allowOrigin = allowedOrigins.includes(origin) ? origin : '';
+
+    // Check if the cache is still valid
     if (Date.now() - cache.timestamp < CACHE_DURATION) {
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': allowOrigin,
+          'Content-Type': 'application/json',
+        },
         body: JSON.stringify({ totalDownloads: cache.downloads }),
       };
     }
 
-    // Lade die Daten neu, wenn der Cache abgelaufen ist
+    // Fetch data again if the cache has expired
     const totalDownloads = await fetchTotalDownloads(packages);
 
-    // Cache aktualisieren
+    // Update the cache
     cache = {
       downloads: totalDownloads,
       timestamp: Date.now(),
@@ -28,10 +37,21 @@ exports.handler = async () => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': allowOrigin,
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({ totalDownloads }),
     };
   } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow all origins
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ error: error.message }),
+    };
   }
 };
 
